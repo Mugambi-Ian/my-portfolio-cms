910b692a8d3ece12d657a8e6f6cd730726bab27d
Ian Mugambi <linksian63@gmail.com>
2023-10-05 15:57:21 +0300

    chore: refactor code

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-docker rm portfolio-cms -f
-docker rmi portfolio-cms -f
-docker build -t portfolio-cms . --network="host"
-docker run -d --name "portfolio-cms"  -i -t -p 1337:1337 portfolio-cms
+docker rm "portfolio.cms" -f
+docker rmi "portfolio.cms" -f
+docker build -t "portfolio-cms" . --network="portfolio-network"
+docker run -d --name "portfolio-cms" --network portfolio-network -i -t -p 1337:1337 portfolio.cms
