6ebe718f5f4e7dcfc46e441b2173664c6181946d
Ian Mugambi <linksian63@gmail.com>
2023-11-07 07:41:49 +0300

    chore: update docker config

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 docker rm "portfolio.cms" -f
 docker rmi "portfolio.cms" -f
 docker-compose build
-docker run -d --network portfolio-network -i -t -p 172.18.0.1:1337:1337 portfolio.cms
+docker run -d --restart unless-stopped --network portfolio-network  --name "portfolio.cms" -i -t -p 172.18.0.1:1337:1337 portfolio.cms
